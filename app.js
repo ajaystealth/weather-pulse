d35aebbdd5d8c3cec3c8beadc23b3d178a4e7529
Ajay Pratap Singh <ajayjnu99@gmail.com>
2025-02-25 22:40:15 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const app = express();
+
+app.use(cors());
+
+
+app.use(cors({ origin: '*'}));
+
+// Serve static files from the root directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set up your server routes
+app.get('/api/getApiKey', (req, res) => {
+  const apiKey = process.env.API_KEY;
+  res.json({ apiKey });
+});
+
+app.get('/api/getUnsplashKey', (req, res) => {
+  const unsplashKey = process.env.UNSPLASH_API_KEY;
+  res.json({ unsplashKey });
+});
+
+// You can add more routes here
+
+// Serve the index.html for any route
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
